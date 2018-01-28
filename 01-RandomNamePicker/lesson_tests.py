5d4370c99e81422c4ab993549c50fc49ec1e115d
Joey Ewbank <jdewbank@gmail.com>
2018-01-28 14:20:35 -0600

    Added code directly as instructed

--- 01-RandomNamePicker/lesson_tests.py
+++ 01-RandomNamePicker/lesson_tests.py
@@ -1,1 +1,6 @@
-print("test")
+class AdditionTests(unittest.TestCase):
+    def test_add_is_a_function(self):
+        self.assertTrue(callable(add))
+
+    def test_adds_two_positive_numbers(self):
+        self.assertEqual(add(1, 1), 2)
