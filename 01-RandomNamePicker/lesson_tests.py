87599d52b4e1b131269264dbd4503d8778b86ca3
Joey Ewbank <jdewbank@gmail.com>
2018-01-28 14:09:48 -0600

    trying different test

--- 01-RandomNamePicker/lesson_tests.py
+++ 01-RandomNamePicker/lesson_tests.py
@@ -1,8 +1,1 @@
-import unittest
-
-class AdditionTests(unittest.TestCase):
-    def test_add_is_a_function(self):
-        self.assertTrue(True)
-
-    def test_adds_two_positive_numbers(self):
-        self.assertEqual(2, 2)
+assert 1==1
